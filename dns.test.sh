872f592f0b68aed6989646ee60e95b7f9b5ad6af
smithwhere <96646318+smithwhere@users.noreply.github.com>
2026-02-27 08:06:44 +0800

    Update dns.test.sh

--- dns.test.sh
+++ dns.test.sh
@@ -14,7 +14,7 @@ TIMEOUT=2          # 超时时间
 DEFAULT_DNS_SERVERS=("1.1.1.1" "8.8.8.8" "9.9.9.9" "208.67.222.222" "199.85.126.10" "84.200.69.80" "1.1.1.2" "64.6.64.6")
 
 # 内置 域名 列表
-DEFAULT_DOMAINS=("www.gstatic.com" "www.youtube.com" "www.google.com")
+DEFAULT_DOMAINS=("www.gstatic.com" "www.google.com" "www.youtube.com")
 # ===========================================
 
 # 颜色定义
